edb50771660a4b4af419fe71fb92240720225293
CowboyLynk <cowboylynk@icloud.com>
2018-05-06 17:18:57 -0400

    cleaned up code and website

---
+++ add_to_database.py
@@ -0,0 +1,86 @@
+import sqlite3
+import datetime
+from enum import Enum
+import random
+import math
+
+
+fish_database = "fish_database.db"  # just come up with name of database
+
+
+class Tables(Enum):
+    temperature = 1
+    turbidity = 2
+    waterlevel = 3
+    ph = 4
+
+
+def db_create():
+    connection = sqlite3.connect(fish_database)
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE temperature (temperature float, timing timestamp);')
+    cursor.execute('CREATE TABLE turbidity (turbidity float, timing timestamp);')
+    cursor.execute('CREATE TABLE waterlevel (waterlevel float, timing timestamp);')
+    cursor.execute('CREATE TABLE ph (ph float, timing timestamp);')
+    connection.commit()
+    connection.close()
+
+
+def db_insert(table, value):
+    connection = sqlite3.connect(fish_database)
+    cursor = connection.cursor()
+
+    if table == Tables.ph:
+        cursor.execute('''INSERT into ph VALUES (?,?);''', (value, datetime.datetime.now()))
+    elif table == Tables.temperature:
+        cursor.execute('''INSERT into temperature VALUES (?,?);''', (value, datetime.datetime.now()))
+    elif table == Tables.waterlevel:
+        cursor.execute('''INSERT into waterlevel VALUES (?,?);''', (value, datetime.datetime.now()))
+    elif table == Tables.turbidity:
+        cursor.execute('''INSERT into turbidity VALUES (?,?);''', (value, datetime.datetime.now()))
+
+    connection.commit()
+    connection.close()
+
+
+def db_lookup(table):
+    connection = sqlite3.connect(fish_database)
+    cursor = connection.cursor()
+
+    time_delta = datetime.datetime.now() - datetime.timedelta(hours=24)
+
+    things = []
+
+    if table == Tables.ph:
+        things = cursor.execute('''SELECT * FROM ph WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+    elif table == Tables.temperature:
+        things = cursor.execute('''SELECT * FROM temperature WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+    elif table == Tables.waterlevel:
+        things = cursor.execute('''SELECT * FROM waterlevel WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+    elif table == Tables.turbidity:
+        things = cursor.execute('''SELECT * FROM turbidity WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+
+    connection.commit()
+    connection.close()
+
+    return things
+
+
+try:
+    db_create()
+except:
+    pass
+for x in range(100):  # pick a 100 datapoints
+    rand = random.randint(-10, 10)
+    y = 85 + 30*math.sin(x/30) + rand
+    y2 = random.randint(0, 100)
+    y3 = 30 + -1*x + rand/2
+    y4 = y2
+
+    db_insert(Tables(1), y)
+    db_insert(Tables(2), y2)
+    db_insert(Tables(3), y3)
+    db_insert(Tables(4), y4)
+
+
+# request_handler({"method": "POST", "values": {"temp": 74}})
---
+++ server_code.py
@@ -0,0 +1,202 @@
+import sqlite3
+import datetime
+from bokeh.plotting import figure
+from bokeh.models import HoverTool, LinearColorMapper
+from bokeh.embed import components
+import numpy as np
+from math import factorial
+from enum import Enum
+import sys
+
+sys.path.append('__HOME__/final_project/')  # changes the directory so that I can call other python scripts
+import alerts
+
+# fish_database = "fish_database.db"  # just come up with name of database
+fish_database = "__HOME__/final_project/fish_database.db"
+
+temperature_low = 72 # https://en.wikipedia.org/wiki/Serpae_tetra
+temperature_high = 79 # https://en.wikipedia.org/wiki/Serpae_tetra
+turbidy_low = 4.2 # Experimental
+ph_low = 6.5  # https://www.fishlore.com/Profiles-SerpaeTetra.htm
+ph_high = 6.8 # https://www.fishlore.com/Profiles-SerpaeTetra.htm
+waterlevel = 2 # Experimental
+
+
+class Tables(Enum):
+    temperature = 1
+    turbidity = 2
+    waterlevel = 3
+    ph = 4
+
+
+def db_create():
+    connection = sqlite3.connect(fish_database)
+    cursor = connection.cursor()
+    cursor.execute('CREATE TABLE temperature (temperature float, timing timestamp);')
+    cursor.execute('CREATE TABLE turbidity (turbidity float, timing timestamp);')
+    cursor.execute('CREATE TABLE waterlevel (waterlevel float, timing timestamp);')
+    cursor.execute('CREATE TABLE ph (ph float, timing timestamp);')
+    connection.commit()
+    connection.close()
+
+
+def db_insert(table, value):
+    connection = sqlite3.connect(fish_database)
+    cursor = connection.cursor()
+
+    if table == Tables.ph:
+        cursor.execute('''INSERT into ph VALUES (?,?);''', (value, datetime.datetime.now()))
+    elif table == Tables.temperature:
+        cursor.execute('''INSERT into temperature VALUES (?,?);''', (value, datetime.datetime.now()))
+    elif table == Tables.waterlevel:
+        cursor.execute('''INSERT into waterlevel VALUES (?,?);''', (value, datetime.datetime.now()))
+    elif table == Tables.turbidity:
+        cursor.execute('''INSERT into turbidity VALUES (?,?);''', (value, datetime.datetime.now()))
+
+    connection.commit()
+    connection.close()
+
+
+def db_lookup(table):
+    connection = sqlite3.connect(fish_database)
+    cursor = connection.cursor()
+
+    time_delta = datetime.datetime.now() - datetime.timedelta(days=24)  # change this to 24 hours in actual project
+
+    things = []
+
+    if table == Tables.ph:
+        things = cursor.execute('''SELECT * FROM ph WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+    elif table == Tables.temperature:
+        things = cursor.execute('''SELECT * FROM temperature WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+    elif table == Tables.waterlevel:
+        things = cursor.execute('''SELECT * FROM waterlevel WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+    elif table == Tables.turbidity:
+        things = cursor.execute('''SELECT * FROM turbidity WHERE timing > ? ORDER BY timing ASC;''', (time_delta,)).fetchall()
+
+    connection.commit()
+    connection.close()
+
+    return things
+
+
+def savgol_filter(y, window_size, order, deriv=0, rate=1):
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # pre-compute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate**deriv * factorial(deriv)
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs(y[1:half_window+1][::-1] - y[0])
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve(m[::-1], y, mode='valid')
+
+
+def make_html(table):
+    entries = db_lookup(Tables(table))
+    x = []
+    y = []
+    for entry in entries:
+        temp, time = entry
+        x.append(datetime.datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f"))
+        y.append(temp)
+    y_prime = np.array(y)
+    y_prime = savgol_filter(y_prime, 51, 3)
+
+    p = figure(x_axis_type="datetime", plot_width=800, plot_height=300)
+
+    color_mapper = LinearColorMapper(palette='Magma256', low=min(y), high=max(y))
+
+    if table == 1:
+        color_mapper = LinearColorMapper(palette='Magma256', low=min(y), high=max(y))
+    else:
+        blues = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#084594']
+        color_mapper = LinearColorMapper(palette=blues, low=min(y), high=max(y))
+    p.scatter(x, y, color={'field': 'y', 'transform': color_mapper}, size=12, line_color="#333333")
+    p.line(x, y_prime, line_dash="5 7", line_width=2, color='gray')
+    # cr = p.circle(x, y, size=20, hover_fill_color="firebrick",
+    #               fill_alpha=0.2, hover_alpha=0.6,
+    #               line_color=None, hover_line_color="white")
+
+    # p.add_tools(HoverTool(tooltips=None, renderers=[cr], mode='vline'))
+    p.yaxis.axis_label = str(Tables(table)).split('.')[1]
+    p.xaxis.axis_label = "Time"
+    p.toolbar.logo = None
+    p.toolbar_location = None
+    p.sizing_mode = 'scale_both'
+
+    script, div = components(p)
+
+    with open("__HOME__/final_project/graphs.html") as output:
+        output = output.read().format(Tables(table).name.title(), script, div)
+
+    return output
+
+
+def request_handler(request):
+    if request["method"] == "GET":  # this means that the user is requesting the webpage
+        table = request["values"].get("table", 1)
+        return make_html(int(table))
+    else:  # the user is posting some data about the fish tank
+        value = float(request["values"]["value"])
+        sensor = Tables(int(request["values"]["sensor"]))
+        db_insert(sensor, value)
+
+        alert = False
+
+        if sensor == Tables.ph:
+            if not ph_low <= value <= ph_high:
+                alert = True
+        elif sensor == Tables.temperature:
+            if not temperature_low <= value <= temperature_high:
+                alert = True
+        elif sensor == Tables.waterlevel:
+            if waterlevel <= value:
+                alert = True
+        elif sensor == Tables.turbidity:
+            if turbidy_low >= value:
+                alert = True
+
+        if not ph_low <= float(db_lookup(Tables(4))[-1:][0][0]) <= ph_high or not temperature_low <= float(db_lookup(Tables(1))[-1:][0][0]) <= temperature_high or waterlevel <= float(db_lookup(Tables(3))[-1:][0][0]) or  turbidy_low >= float(db_lookup(Tables(2))[-1:][0][0]):
+            alert = True
+
+        if alert:
+            alerts.alert_all(temperature_high)
+            return "ON"
+        return "OFF"
+
+
+# db_create()
+# db_insert(Tables(1), 77.8)
+# db_insert(Tables(1), 78.9)
+# db_insert(Tables(1), 77.7)
+# db_insert(Tables(1), 77.4)
+# db_insert(Tables(1), 77.2)
+# db_insert(Tables(2), 4.98)
+# db_insert(Tables(2), 4.98)
+# db_insert(Tables(2), 4.96)
+# db_insert(Tables(2), 4.97)
+# db_insert(Tables(2), 4.95)
+# db_insert(Tables(3), 0.51)
+# db_insert(Tables(3), 0.72)
+# db_insert(Tables(3), 0.52)
+# db_insert(Tables(3), 1.22)
+# db_insert(Tables(3), 0.93)
+# db_insert(Tables(4), 6.62)
+# db_insert(Tables(4), 6.70)
+# db_insert(Tables(4), 6.66)
+# db_insert(Tables(4), 6.69)
+# db_insert(Tables(4), 6.72)
+
+# request_handler({"method": "POST", "values": {"temp": 74}})
